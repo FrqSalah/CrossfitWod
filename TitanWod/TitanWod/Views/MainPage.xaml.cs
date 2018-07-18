49c1e10a4b40f42a6be329d145850dd7e930bc40
FrqSalah <frq.salah@gmail.com>
2018-07-18 23:00:59 +0200

    Fonctionnal Commit

--- TitanWod/TitanWod/Views/MainPage.xaml.cs
+++ TitanWod/TitanWod/Views/MainPage.xaml.cs
@@ -2,15 +2,33 @@
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using TitanWod.Helpers.MenuItems;
 
 namespace TitanWod.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
-	{
+	public partial class MainPage : MasterDetailPage
+    {
 		public MainPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            masterPage.ListView.ItemSelected += OnItemSelected;
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                var item = e.SelectedItem as MasterPageItem;
+                if (item != null)
+                {
+                    Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                    masterPage.ListView.SelectedItem = null;
+                    IsPresented = false;
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
 }
